6a9a1d085972f225bc2022faf5bf91a5586671b4
planet95 <planet95@gmail.com>
2014-01-10 18:53:23 -0800

    votelist.js

---
+++ public/js/votelist.js
@@ -0,0 +1,9 @@
+﻿ var socket = null;
+ var uname = null;
+ $(function () {
+     socket = io.connect();
+    // socket.emit('ready', uname);
+    /*
+    create room when new id is added onto url. index will show list of rooms on server. admin will show results from each room. 
+    */
+});
